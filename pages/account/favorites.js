d509d15469396ce7b3cf32e2778a22330326f204
amerejil <amerejil@outlook.com>
2021-04-30 22:16:40 -0500

    :)

--- pages/account/favorites.js
+++ pages/account/favorites.js
@@ -4,30 +4,36 @@ import { getFavoriteApi } from "../../Api/favorite";
 import useAuth from "../../hooks/useAuth";
 import ListProducts from "../../components/ListProducts";
 import { Loader } from "semantic-ui-react";
-
+import { useRouter } from "next/router";
+import Header from "../../components/header";
+import useCategories from "../../hooks/useCategories";
 export default function favorites() {
   const { auth, logout } = useAuth();
   const [games, setgames] = useState(null);
-
+  const { categorias } = useCategories();
+  const router = useRouter();
   useEffect(() => {
     (async () => {
-      const response = await getFavoriteApi(auth.idUser, logout);
-      console.log(response);
+      const response = await getFavoriteApi(auth?.idUser, logout);
       if (size(response) > 0) {
         const gamesList = [];
         forEach(response, (data) => {
           gamesList.push(data.producto);
         });
-        console.log(gamesList);
+
         setgames(gamesList);
       } else {
         setgames([]);
       }
     })();
   }, []);
-  console.log(auth);
+  if (!auth) {
+    router.replace("/");
+    return null;
+  }
   return (
     <div className="whishlist_block">
+      <Header categorias={categorias}> </Header>
       <div className="title">Lista de favoritos</div>
       <div className="data">
         {!games && <Loader active> Cargando juegos</Loader>}
