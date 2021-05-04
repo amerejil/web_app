b36e46d83cf1e227be0c042aa04113e1a9502d7f
amerejil <amerejil@outlook.com>
2021-05-04 00:38:51 -0500

    :)

--- components/SearchCard/SearchCard.js
+++ components/SearchCard/SearchCard.js
@@ -1,18 +1,24 @@
 import React from "react";
-
+import Link from "next/link";
 export default function SearchCard(props) {
   const { products } = props;
 
   if (!products.length > 0) return null;
   return (
-    <div className="searchCard">
-      {products?.map((product) => (
-        <div className="one-product" key={product.id}>
-          <img src={product.imagen.formats.thumbnail.url}></img>
-          <span className="title">{product.title}</span>
-          <span className="precio">${product.price}</span>
-        </div>
-      ))}
+    <div className="searchCardAbsolute">
+      <div className="searchCard">
+        {products?.map((product) => (
+          <Link href={`/${product.url}`}>
+            <a>
+              <div className="one-product" key={product.id}>
+                <img src={product.imagen.formats.thumbnail.url}></img>
+                <span className="title">{product.title}</span>
+                <span className="precio">${product.price}</span>
+              </div>
+            </a>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
