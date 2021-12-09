b32713e3bd6d03f459536c249afd12c4743f3560
amerejil <amerejil@outlook.com>
2021-12-09 10:38:16 -0500

    configurando backend

--- Utils/constants.js
+++ Utils/constants.js
@@ -1,3 +1,3 @@
-export const BASE_PATH = "https://lx-web-server.herokuapp.com";
+export const BASE_PATH = "https://ferrecity.com.ec/backend-web";
 export const TOKEN = "token";
 export const CART = "cart";
